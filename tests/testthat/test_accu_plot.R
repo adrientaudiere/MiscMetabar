376d18b24afa0051453abfc86f373460255bad68
Adrien Taudiere <adrien.taudiere@tutanota.com>
2022-05-03 09:33:53 +0200

    add some test

---
+++ tests/testthat/test_accu_plot.R
@@ -0,0 +1,6 @@
+data("GlobalPatterns")
+GP <- subset_taxa(GlobalPatterns, GlobalPatterns@tax_table, 1 == 'Archaea')
+expect_s3_class()
+
+accu_plot(GP, 'SampleType', nb_seq = TRUE, by.fact = TRUE)
+
