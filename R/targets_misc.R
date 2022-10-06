f40c9b8ac00e14b0508b7d13e07b98055dec1db3
Adrien Taudiere <adrien.taudiere@tutanota.com>
2022-10-06 16:37:47 +0200

    document MM_idtaxa

--- R/targets_misc.R
+++ R/targets_misc.R
@@ -1,6 +1,22 @@
 
-# MM_idtaxa(seqtab, train_maarjam, type = "collapsed")
-MM_idtaxa <- function(test, trainingSet, ...) {
+################################################################################
+#' A wrapper of \code{\link[DECIPHER]{IdTaxa}} which result in a tibble with 
+#' @description
+#' `r lifecycle::badge("experimental")`
+#' @param test (required): 
+#' @param trainingSet (required): 
+#' @param column_names (optional but often needed): names for the 
+#' column of the resulted tibble
+#' @param ... Additional arguments passed on to \code{\link[DECIPHER]{IdTaxa}}
+#'
+#' @examples
+#' @author Adrien Taudière
+#'
+#' @return a tibble that can be used for tax_table
+#'
+#' @export
+
+MM_idtaxa <- function(test, trainingSet, column_names = c("Kingdom_idtaxa", "Phyla_idtaxa", "Class_idtaxa", "Order_idtaxa", "Family_idtaxa", "Genus_idtaxa", "Species_idtaxa", "VT_idtaxa"), ...) {
     idtaxa <- DECIPHER::IdTaxa(test = test, trainingSet = trainingSet, ...)
     col2add <- 8 - lengths(regmatches(idtaxa, gregexpr(";", idtaxa)))
     for (i in seq_along(idtaxa)) {
@@ -15,15 +31,6 @@ MM_idtaxa <- function(test, trainingSet, ...) {
     t_idtaxa <- tibble::tibble(data.frame(
         stringr::str_split_fixed(idtaxa, ";", 9)
     ))
-    colnames(t_idtaxa) <- c(
-        "Kingdom_idtaxa",
-        "Phyla_idtaxa",
-        "Class_idtaxa",
-        "Order_idtaxa",
-        "Family_idtaxa",
-        "Genus_idtaxa",
-        "Species_idtaxa",
-        "VT_idtaxa"
-    )
+    colnames(t_idtaxa) <- column_names
     return(t_idtaxa)
 }
