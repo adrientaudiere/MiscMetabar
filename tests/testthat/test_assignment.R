15568de2f710f2f81a63c42592e5ce4c6a5c700b
Adrien Taudiere <6680165+adrientaudiere@users.noreply.github.com>
2026-01-12 16:26:52 +0100

    Refactor tests for assign_blastn and add TODOs
    Updated tests for assign_blastn and added TODOs for other functions.

--- tests/testthat/test_assignment.R
+++ tests/testthat/test_assignment.R
@@ -2,31 +2,38 @@ data(data_fungi)
 
 test_that("assign_blastn works", {
   skip_on_cran()
-  # Create a simple DNA string set for testing
-  dna <- refseq(data_fungi)[1:5]
+  ref_fasta <- Biostrings::readDNAStringSet(system.file("extdata",
+  "mini_UNITE_fungi.fasta.gz",
+  package = "MiscMetabar", mustWork = TRUE
+))
+
+# assign_blastn(data_fungi_mini, ref_fasta = ref_fasta) # error because not
+# enough sequences in db so none blast query passed the filters.
+# So we used low score filter hereafter.
+
+mat <- assign_blastn(data_fungi_mini,
+  ref_fasta = ref_fasta,
+  method_algo = "top-hit", min_id = 70, min_e_value = 1e-3, min_cover = 50,
+  min_bit_score = 20
+)
+  
+expect_equal(dim(mat), c(41, 9))
+  
   expect_error(assign_blastn(dna, database = "non_existent_db"))
 })
 
 test_that("assign_dada2 works", {
-  skip_on_cran()
-  dna <- refseq(data_fungi)[1:5]
-  # This will fail without a proper database, but we test that the function exists
-  expect_error(assign_dada2(dna, database = "non_existent_db"))
+  #TODO
 })
 
 test_that("assign_idtaxa works", {
-  skip_on_cran()
   if (requireNamespace("DECIPHER", quietly = TRUE)) {
-    dna <- refseq(data_fungi)[1:5]
-    # This will fail without a proper trained set, but we test that the function exists
-    expect_error(assign_idtaxa(dna, trained_set = "non_existent_set"))
+  #TODO
   }
 })
 
 test_that("learn_idtaxa works", {
-  skip_on_cran()
   if (requireNamespace("DECIPHER", quietly = TRUE)) {
-    # This will fail without proper input, but we test that the function exists
-    expect_error(learn_idtaxa(seqs = NULL, taxonomy = NULL))
+  #TODO
   }
 })
