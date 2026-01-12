1d6c94d5a554ef72d0a4e95a9a903517ed85f97e
Adrien Taudiere <6680165+adrientaudiere@users.noreply.github.com>
2026-01-12 18:11:10 +0100

    Remove skip_on_cran from deprecated function test
    Removed skip_on_cran from clean_physeq test.

--- tests/testthat/test_deprecated_funcs.R
+++ tests/testthat/test_deprecated_funcs.R
@@ -1,7 +1,6 @@
 data(data_fungi)
 
 test_that("clean_physeq deprecated function works", {
-  skip_on_cran()
   expect_warning(result <- clean_physeq(data_fungi), "deprecated")
   expect_s4_class(result, "phyloseq")
 })
