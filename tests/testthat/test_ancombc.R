e44dd7637f56c298bea7100a9428a5f9d89e3c8f
Adrien Taudiere <6680165+adrientaudiere@users.noreply.github.com>
2026-01-12 16:12:19 +0100

    Refactor ANCOMBC test cases for clarity and efficiency
    Refactor ANCOMBC tests to streamline data handling and improve clarity.

--- tests/testthat/test_ancombc.R
+++ tests/testthat/test_ancombc.R
@@ -1,23 +1,20 @@
 data(data_fungi)
-data(enterotype)
+data_subset <- subset_samples(data_fungi, Height %in% c("Low", "High")) |>
+  clean_pq()
+
+ancombc_res <- ancombc_pq(data_subset, "Height")
 
 test_that("signif_ancombc works", {
-  skip_on_cran()
-  if (requireNamespace("ANCOMBC", quietly = TRUE)) {
-    data_subset <- subset_samples(data_fungi, Height %in% c("Low", "High"))
-    # Run ancombc first
-    ancombc_res <- ancombc_pq(data_subset, "Height")
+  if (requireNamespace("ANCOMBC", quietly = TRUE)) {    
+    expect_type(ancombc_res, "list")
     result <- signif_ancombc(ancombc_res)
     expect_s3_class(result, "data.frame")
   }
 })
 
 test_that("plot_ancombc_pq works", {
-  skip_on_cran()
   if (requireNamespace("ANCOMBC", quietly = TRUE)) {
-    data_subset <- subset_samples(data_fungi, Height %in% c("Low", "High"))
-    ancombc_res <- ancombc_pq(data_subset, "Height")
-    p <- plot_ancombc_pq(ancombc_res, data_subset)
+    p <- plot_ancombc_pq(data_subset, ancombc_res)
     expect_s3_class(p, "ggplot")
   }
 })
