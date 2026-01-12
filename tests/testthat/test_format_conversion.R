f35963afa4ce41d56990a1a04e18a5d8ae21350a
Adrien Taudiere <6680165+adrientaudiere@users.noreply.github.com>
2026-01-12 18:19:48 +0100

    Remove tests and add TODOs for format functions
    Removed tests for format2dada2, format2dada2_species, and format2sintax functions. Added TODO comments for future implementation.

--- tests/testthat/test_format_conversion.R
+++ tests/testthat/test_format_conversion.R
@@ -1,25 +1,13 @@
 data(data_fungi)
 
 test_that("format2dada2 works", {
-  skip_on_cran()
-  # Test with simple taxonomy
-  tax <- c("k__Fungi", "p__Ascomycota", "c__Dothideomycetes")
-  result <- format2dada2(tax)
-  expect_type(result, "character")
+TODO
 })
 
 test_that("format2dada2_species works", {
-  skip_on_cran()
-  # Test with species level taxonomy
-  tax <- c("k__Fungi", "p__Ascomycota", "c__Dothideomycetes", "s__Species_name")
-  result <- format2dada2_species(tax)
-  expect_type(result, "character")
+TODO
 })
 
 test_that("format2sintax works", {
-  skip_on_cran()
-  # Test with simple taxonomy
-  tax <- c("Kingdom", "Phylum", "Class")
-  result <- format2sintax(tax)
-  expect_type(result, "character")
+TODO
 })
