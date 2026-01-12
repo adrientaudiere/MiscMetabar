8dc152e27a23de1811678770ef16e0e36d1cdb03
Adrien Taudiere <6680165+adrientaudiere@users.noreply.github.com>
2026-01-12 18:39:49 +0100

    Refactor tests in test_taxonomy_utils.R
    Removed skip_on_cran calls and updated taxa_only_in_one_level test.

--- tests/testthat/test_taxonomy_utils.R
+++ tests/testthat/test_taxonomy_utils.R
@@ -2,27 +2,23 @@ data(data_fungi)
 data(enterotype)
 
 test_that("taxa_as_columns works", {
-  skip_on_cran()
   result <- taxa_as_columns(data_fungi)
   expect_s4_class(result, "phyloseq")
   expect_false(taxa_are_rows(result))
 })
 
 test_that("taxa_as_rows works", {
-  skip_on_cran()
   result <- taxa_as_rows(data_fungi)
   expect_s4_class(result, "phyloseq")
   expect_true(taxa_are_rows(result))
 })
 
 test_that("taxa_only_in_one_level works", {
-  skip_on_cran()
-  result <- taxa_only_in_one_level(data_fungi, "Height")
-  expect_s4_class(result, "phyloseq")
+  result <- taxa_only_in_one_level(data_fungi, "Height", "Low")
+  expect_equal(length(result), 124)
 })
 
 test_that("tbl_sum_taxtable works", {
-  skip_on_cran()
   if (requireNamespace("gtsummary", quietly = TRUE)) {
     result <- tbl_sum_taxtable(data_fungi)
     expect_s3_class(result, "tbl_summary")
