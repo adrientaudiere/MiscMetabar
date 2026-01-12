64bbb694ab5299f8a29eebdc7a6c86db65fdbba0
Adrien Taudiere <6680165+adrientaudiere@users.noreply.github.com>
2026-01-12 15:11:20 +0100

    Refactor alpha diversity tests with data subset
    Refactor tests for hill curves and hill test functions to use a cleaned data subset.

--- tests/testthat/test_alpha_div.R
+++ tests/testthat/test_alpha_div.R
@@ -1,18 +1,18 @@
 data(data_fungi)
-data(enterotype)
+
+data_subset <- subset_samples(data_fungi, Height %in% c("Low", "High")) |>
+  rarefy_even_depth() |> 
+  clean_pq()
 
 test_that("hill_curves_pq works", {
-  skip_on_cran()
   if (requireNamespace("iNEXT", quietly = TRUE)) {
-    data_subset <- subset_samples(data_fungi, Height %in% c("Low", "High"))
     result <- hill_curves_pq(data_subset, "Height")
     expect_s3_class(result, "ggplot")
   }
 })
 
 test_that("hill_test_rarperm_pq works", {
-  skip_on_cran()
-  data_subset <- subset_samples(data_fungi, Height %in% c("Low", "High"))
   result <- hill_test_rarperm_pq(data_subset, "Height", nperm = 9)
   expect_type(result, "list")
+  expect_s3_class(result[[1]], "statsExpressions")
 })
